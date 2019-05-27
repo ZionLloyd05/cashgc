df99e8e783c1fa68e24fc64f8addb609150ba750
ZionLloyd05 <zionlloyd05@gmail.com>
2019-05-27 11:24:27 +0100

    added admin route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,11 @@
+import { AdminRoute } from "./admin";
 import { UserRoute } from "./user";
 import { PublicRoute } from "./public.route";
 import { PingRoute } from "./ping";
 
-export const ROUTERS = [new PingRoute(), new PublicRoute(), new UserRoute()];
+export const ROUTERS = [
+  new PingRoute(),
+  new PublicRoute(),
+  new UserRoute(),
+  new AdminRoute()
+];
